ccef57133a837ff4178faae5fb20794a4fb3504a
Luisa Zanardini <106076128+lissS2@users.noreply.github.com>
2024-04-30 11:20:49 -0300

    .

--- main.js
+++ main.js
@@ -35,4 +35,13 @@ function calculaTempo(tempoObjetivo1){
     let minutos = Math.floor(segundos / 60);
     let horas = Math.floor(minutos / 60);
     let dias = Math.floor(horas / 24);
+        
+        segundos %= 60;
+        minutos %= 60;
+        hora %= 24;
+
+return dias + "dias"
+         + horas + "horas"
+         + minutos + "minutos"
+         + segundos + "segundos"
 }
